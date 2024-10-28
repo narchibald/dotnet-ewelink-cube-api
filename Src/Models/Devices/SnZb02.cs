0518c764ab373ce8f053afef4db674d5e5d24e43
Nick Archibald <n_archibald@outlook.com>
2024-10-28 20:45:17 +1300

    Added devices; mirco, SNZB-02

---
+++ Src/Models/Devices/SnZb02.cs
@@ -0,0 +1,8 @@
+using EWeLink.Cube.Api.Models.States;
+
+namespace EWeLink.Cube.Api.Models.Devices;
+
+[SubDeviceIdentifier("ZCL_HA_DEVICEID_TEMPERATURE_SENSOR")]
+public class SnZb02 : SubDevice<TemperatureAndHumiditySensor>
+{
+}
--- Src/Models/States/ZbMicroState.cs
+++ Src/Models/States/ZbMicroState.cs
@@ -3,19 +3,13 @@ using Newtonsoft.Json;
 
 namespace EWeLink.Cube.Api.Models.States;
 
-public class ZbMicroState : SubDeviceState
+public class ZbMicroState : MicroState
 {
     [JsonProperty("transmit-power")]
     public TransmitPowerCapability? TransmitPower { get; set; }
     
     [JsonProperty("startup")]
     public StartupCapability? Startup { get; set; }
-    
-    [JsonProperty("power")]
-    public PowerCapability? Power { get; set; }
-    
-    [JsonProperty("rssi")]
-    public RssiCapability? Rssi { get; set; }
     
     [JsonProperty("lqi")]
     public LqiCapability? Lqi { get; set; }
